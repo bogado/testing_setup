b0cc895af1e11cc6ed9cc3a65ff097b40a40f1f4
Victor Bogado da Silva Lins <victor@bogado.net>
2025-01-24 11:06:19 -0500

    Make the way back from bytes.

--- msgpack/byte_view.hpp
+++ msgpack/byte_view.hpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <bit>
 #include <concepts>
+#include <ranges>
 #include <type_traits>
 
 namespace vb {
@@ -17,6 +18,11 @@ constexpr auto byte_view(const is_basic_type auto& value) {
     return std::bit_cast<std::array<std::byte, sizeof(value)>>(value);
 }
 
+template <is_basic_type TYPE, std::ranges::sized_range DATA_VIEW>
+requires(sizeof(std::ranges::range_value_t<DATA_VIEW>) == 1)
+TYPE from_bytes(const DATA_VIEW& byte_view)
+{
+    return std::bit_cast<TYPE>(byte_view);
 }
 
 #endif // INCLUDED_BYTE_VIEW_HPP
